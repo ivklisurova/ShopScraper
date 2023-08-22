82f86d0b4b34d367441a467127ccded5b41cf52b
Ivanka Klisurova <55747390+ivklisurova@users.noreply.github.com>
2023-08-22 11:02:35 +0300

    add both links from attachment, fixing typos

--- shopscraper/shopscraper/spiders/shopspider.py
+++ shopscraper/shopscraper/spiders/shopspider.py
@@ -9,7 +9,10 @@ from ..items import ShopscraperItem
 
 
 class ShopSpider(scrapy.Spider):
-    name = "sspider"
+    name = 'sspider'
+    allowed_domains = ['shop.mango.com']
+    start_urls = ['https://shop.mango.com/gb/women/skirts-midi/midi-satin-skirt_17042020.html?c=99',
+                  'https://shop.mango.com/bg-en/men/t-shirts-plain/100-linen-slim-fit-t-shirt_47095923.html?c=07']
 
     @staticmethod
     def get_product_code(address):
@@ -21,34 +24,35 @@ class ShopSpider(scrapy.Spider):
         return product_code, product_id
 
     def start_requests(self):
-        url = 'https://shop.mango.com/gb/women/skirts-midi/midi-satin-skirt_17042020.html?c=99'
-        item_code = self.get_product_code(url)[0]
-        item_id = self.get_product_code(url)[1]
-        desired_capabilities = DesiredCapabilities.CHROME
-        desired_capabilities['goog:loggingPrefs'] = {'performance': 'ALL'}
-        options = webdriver.ChromeOptions()
-        options.add_argument('--headless=new')
-        options.add_argument('--ignore-certificate-errors')
+        for url in self.start_urls:
+            # url = 'https://shop.mango.com/gb/women/skirts-midi/midi-satin-skirt_17042020.html?c=99'
+            item_code = self.get_product_code(url)[0]
+            item_id = self.get_product_code(url)[1]
+            desired_capabilities = DesiredCapabilities.CHROME
+            desired_capabilities['goog:loggingPrefs'] = {'performance': 'ALL'}
+            options = webdriver.ChromeOptions()
+            options.add_argument('--headless=new')
+            options.add_argument('--ignore-certificate-errors')
 
-        driver = webdriver.Chrome(options=options)
+            driver = webdriver.Chrome(options=options)
 
-        driver.get(url)
-        time.sleep(2)
+            driver.get(url)
+            time.sleep(2)
 
-        logs = driver.get_log('performance')
+            logs = driver.get_log('performance')
 
-        for log in logs:
-            network_log = json.loads(log['message'])['message']
+            for log in logs:
+                network_log = json.loads(log['message'])['message']
 
-            if network_log['method']=='Network.responseReceived':
-                try:
-                    api_url = network_log['params']['response']['url']
-                    if item_code in api_url:
-                        yield Request(url=api_url,
-                                      callback=self.parse, meta={'item_id': item_id})
-                except KeyError:
-                    pass
-        driver.quit()
+                if network_log['method']=='Network.responseReceived':
+                    try:
+                        api_url = network_log['params']['response']['url']
+                        if item_code in api_url:
+                            yield Request(url=api_url,
+                                          callback=self.parse, meta={'item_id': item_id})
+                    except KeyError:
+                        pass
+            driver.quit()
 
     def parse(self, response, **kwargs):
         item = ShopscraperItem()
